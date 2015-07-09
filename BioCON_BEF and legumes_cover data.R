a2fde281f45c93573e78a04e1d2ac1c4f4e86ae3
klapierre <lapierre.kimberly@gmail.com>
2015-07-08 17:29:31 -0700

    Add source files for bar graph stats function and ggplot theme settings. Set working directory.

--- BioCON_BEF and legumes_cover data.R
+++ BioCON_BEF and legumes_cover data.R
@@ -6,3 +6,7 @@ library(nlme)
 library(lsmeans)
 library(lavaan)
 
+source('C:\\Users\\Kim\\Desktop\\general R code\\general-R-code\\bar graph summary stats.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\general R code\\general-R-code\\ggplot_theme set.r', chdir=T)
+
+setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
