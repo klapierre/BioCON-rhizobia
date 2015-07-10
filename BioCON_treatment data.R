7195594ca80e1ee61601df37d524024d9831ac11
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 12:26:37 -0700

    Add column of legume number and spp name for plots with just one legume.

--- BioCON_treatment data.R
+++ BioCON_treatment data.R
@@ -12,5 +12,15 @@ trt$legume_num <- trt$Amorpha.canescens + trt$Lespedeza.capitata + trt$Lupinus.p
 #make a column with legume ID for plots with one legume
 trt$legume_spp <- as.factor(paste(trt$Amorpha.canescens, trt$Lespedeza.capitata, trt$Lupinus.perennis, trt$Petalostemum.villosum, sep='_'))
 
+#make a column with legume spp for one legume plots, "two", "three" or "all 4" for multiple legume plots
+trt$leg_num_spp_initial <- with(trt, as.character(ifelse(Amorpha.canescens==1, 'AMCA',
+                                              ifelse(Lespedeza.capitata==1, 'LECA',
+                                                     ifelse(Lupinus.perennis==1, 'LUPE',
+                                                            ifelse(Petalostemum.villosum==1, 'PEVI', '0 legumes'))))))
+trt$leg_num_spp <- with(trt, as.factor(ifelse(legume_num==2, '2 legumes',
+                                       ifelse(legume_num==3, '3 legumes',
+                                       ifelse(legume_num==4, '4 legumes', leg_num_spp_initial)))))
+trt <- trt[,-33]
+
 #making a combined CO2 and N treatment column
 trt$trt <- paste(trt$CO2_trt, trt$N_trt, sep='_')
