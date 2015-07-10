791a80af522995b73e8e33b2a9457ee056ef78cf
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 14:00:02 -0700

    Merge ANPP and climate data.

---
+++ BioCON_BEF and legumes_anpp analysis.R
@@ -0,0 +1,19 @@
+library(plyr)
+library(ggplot2)
+library(reshape2)
+library(grid)
+library(nlme) 
+library(lsmeans)
+library(lavaan)
+
+source('C:\\Users\\Kim\\Desktop\\general R code\\general-R-code\\bar graph summary stats.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\general R code\\general-R-code\\ggplot_theme set.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data\\BioCON-rhizobia\\BioCON_treatment data.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data\\BioCON-rhizobia\\BioCON_climate data.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data\\BioCON-rhizobia\\BioCON_anpp data.r', chdir=T)
+source('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data\\BioCON-rhizobia\\BioCON_cover data.r', chdir=T)
+
+setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
+
+#merge ANPP and climate data
+anppClimate <- merge(anppRY, climate)
