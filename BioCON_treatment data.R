af1a478a643574ef0a4c20d4ab74e0e9e5742e53
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 11:29:50 -0700

    Make a consistant seperator for each combined column.

--- BioCON_treatment data.R
+++ BioCON_treatment data.R
@@ -4,7 +4,7 @@ setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
 trt <- read.csv('e141_treatments.csv')
 
 #generate a column with binary code for the species treatment for each plot
-trt$spp_trt <- with(trt, as.factor(paste(Achillea.millefolium, Agropyron.repens, Amorpha.canescens, Andropogon.gerardi, Anemone.cylindrica, Asclepias.tuberosa, Bouteloua.gracilis, Bromus.inermis, Koeleria.cristata, Lespedeza.capitata, Lupinus.perennis, Petalostemum.villosum, Poa.pratensis, Schizachyrium.scoparium, Solidago.rigida, Sorghastrum.nutans, sep='.')))
+trt$spp_trt <- with(trt, as.factor(paste(Achillea.millefolium, Agropyron.repens, Amorpha.canescens, Andropogon.gerardi, Anemone.cylindrica, Asclepias.tuberosa, Bouteloua.gracilis, Bromus.inermis, Koeleria.cristata, Lespedeza.capitata, Lupinus.perennis, Petalostemum.villosum, Poa.pratensis, Schizachyrium.scoparium, Solidago.rigida, Sorghastrum.nutans, sep='_')))
 
 #get total number of legumes in each plot
 trt$legume_num <- trt$Amorpha.canescens + trt$Lespedeza.capitata + trt$Lupinus.perennis + trt$Petalostemum.villosum
