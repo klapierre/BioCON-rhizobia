79b8fce4ba1d8ff146a81632739f8b9a87b3e186
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 14:52:07 -0700

    Mixed model relating growing year precip and species ID to the difference between 1 and 4 legume plots for each legume species.

--- BioCON_BEF and legumes_anpp analysis.R
+++ BioCON_BEF and legumes_anpp analysis.R
@@ -17,6 +17,9 @@ setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
 
 #merge ANPP and climate data
 anppClimate <- merge(anppRY, climate)
+polyClimate <- merge(anppPolyRY, climate)
+legume4to1Climate <- merge(legume4to1complete, climate)
+
 
 
 ##############################################
@@ -76,3 +79,36 @@ ggplot(subset(anppClimate, spp_count==4 & trt=='Camb_Namb' & legume_num==1), aes
   ylab('log Relative Yield Total') +
   xlab('Total Annual Precipitation (cm)') +
   scale_colour_manual(values=color)
+
+
+#mixed model
+#effect of other legumes on RY of each legume species
+RYmodel <- lme(logRY ~ gy_precip_cm*other_legs, random=~1|plot, data=subset(polyClimate, spp_count==4 & trt=='Camb_Namb' & legume_num!=0 & legume_num!=2 & legume_num!=3))
+summary(RYmodel)
+anova(RYmodel)
+
+polyClimate$order <- factor(polyClimate$other_legs, levels=c('0 legumes', 'single legume', '2 legumes', '3 legumes', '4 legumes'))
+ggplot(barGraphStats(data=subset(polyClimate, spp_count==4 & trt=='Camb_Namb' & leg_num_spp!='2 legumes' & leg_num_spp!='3 legumes' & Legume==1 & spp_type=='legume'), variable='logRY', byFactorNames=c('species', 'order', 'gy_precip_cm')), aes(x=species, y=mean, fill=order)) +
+  geom_bar(stat='identity', position=position_dodge(), colour='black') +
+  scale_fill_manual(values=c("white", "grey")) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
+  ylab('log Relative Yield') +
+  facet_wrap(~gy_precip_cm, ncol=4) +
+  theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1), axis.title.x=element_blank())
+
+
+#difference between 1 and 4 legume plots
+RYmodelPrecip <- lm(diff ~ gy_precip_cm*species, data=subset(legume4to1Climate, trt=='Camb_Namb'))
+summary(RYmodelPrecip)
+anova(RYmodelPrecip)
+
+color <- c("#E69F00", "#009E73", "#0072B2", "#CC79A7")
+
+ggplot(subset(legume4to1Climate, trt=='Camb_Namb'), aes(x=gy_precip_cm, y=diff, colour=species)) +
+  geom_point(size=2) +
+  geom_smooth(method=lm, size=1, se=F) +
+  xlab('Total Annual Precipitation (cm)') +
+  ylab('Difference in Relative Yield') +
+  scale_colour_manual(values=color,
+                      labels=c('Amorpha canescens', 'Lespedeza capitata', 'Lupinus perennis', 'Petalostemum villosum'))
+
