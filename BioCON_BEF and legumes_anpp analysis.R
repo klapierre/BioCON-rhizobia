1166e549fd3319d47b2964533da526989679e78f
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 14:05:04 -0700

    Mixed model relating growing year precip and legume species number and ID (in one legume plots) to logRYT.

--- BioCON_BEF and legumes_anpp analysis.R
+++ BioCON_BEF and legumes_anpp analysis.R
@@ -17,3 +17,21 @@ setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
 
 #merge ANPP and climate data
 anppClimate <- merge(anppRY, climate)
+
+#mixed model
+#IMPORTANT: 4 spp polycultures
+#IMPORTANT: using growing year climate variables
+RYTmixedCategorical <- lme(logRYT ~ gy_precip_cm*leg_num_spp, random=~1|plot, data=subset(anppClimate, spp_count==4 & trt=='Camb_Namb'))
+summary(RYTmixedCategorical)
+anova(RYTmixedCategorical)
+lsmeans(RYTmixedCategorical, cld~leg_num_spp)
+
+anppClimate$order <- factor(anppClimate$leg_num_spp, levels=c('0 legumes', 'AMCA', 'LECA', 'LUPE', 'PEVI', '2 legumes', '3 legumes', '4 legumes'))
+
+ggplot(subset(anppClimate, spp_count==4 & trt=='Camb_Namb'), aes(x=gy_precip_cm, y=logRYT, colour=order)) +
+  geom_point() +
+  geom_smooth(method=lm) +
+  ylab('log Relative Yield Total') +
+  xlab('Total Annual Precipitation (cm)')
+
+
