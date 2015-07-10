11619c6c9dcd8baafd19623093c8c11503f3b833
klapierre <lapierre.kimberly@gmail.com>
2015-07-10 11:28:52 -0700

    Remove biomass of 16 spp weeds in plots where they are mistakenly labeled as trt spp.

--- BioCON_anpp data.R
+++ BioCON_anpp data.R
@@ -58,8 +58,45 @@ names(anpp)[names(anpp)=="Unsorted.Biomass"] <- "Unsort"
 #merge anpp and trt data
 anppTrt <- merge(anpp, trt, all=T)
 
+#get rid of 16 spp weeds in plots where they are labeled as spp
+anppTrt$ACMI1 <- with(anppTrt, ifelse(Achillea.millefolium==1, ACMI, 0))
+anppTrt$AGRE1 <- with(anppTrt, ifelse(Agropyron.repens==1, AGRE, 0))
+anppTrt$AMCA1 <- with(anppTrt, ifelse(Amorpha.canescens==1, AMCA, 0))
+anppTrt$ANGE1 <- with(anppTrt, ifelse(Andropogon.gerardi==1, ANGE, 0))
+anppTrt$ANCY1 <- with(anppTrt, ifelse(Anemone.cylindrica==1, ANCY, 0))
+anppTrt$ASTU1 <- with(anppTrt, ifelse(Asclepias.tuberosa==1, ASTU, 0))
+anppTrt$BOGR1 <- with(anppTrt, ifelse(Bouteloua.gracilis==1, BOGR, 0))
+anppTrt$BRIN1 <- with(anppTrt, ifelse(Bromus.inermis==1, BRIN, 0))
+anppTrt$KOCR1 <- with(anppTrt, ifelse(Koeleria.cristata==1, KOCR, 0))
+anppTrt$LECA1 <- with(anppTrt, ifelse(Lespedeza.capitata==1, LECA, 0))
+anppTrt$LUPE1 <- with(anppTrt, ifelse(Lupinus.perennis==1, LUPE, 0))
+anppTrt$PEVI1 <- with(anppTrt, ifelse(Petalostemum.villosum==1, PEVI, 0))
+anppTrt$POPR1 <- with(anppTrt, ifelse(Poa.pratensis==1, POPR, 0))
+anppTrt$SCSC1 <- with(anppTrt, ifelse(Schizachyrium.scoparium==1, SCSC, 0))
+anppTrt$SORI1 <- with(anppTrt, ifelse(Solidago.rigida==1, SORI, 0))
+anppTrt$SONU1 <- with(anppTrt, ifelse(Sorghastrum.nutans==1, SONU, 0))
+anppNoTrt <- subset(anppTrt, select=-c(Achillea.millefolium, Agropyron.repens, Amorpha.canescens, Andropogon.gerardi, Anemone.cylindrica, Asclepias.tuberosa, Bouteloua.gracilis, Bromus.inermis, Koeleria.cristata, Lespedeza.capitata, Lupinus.perennis, Petalostemum.villosum, Poa.pratensis, Schizachyrium.scoparium, Solidago.rigida, Sorghastrum.nutans, CO2_trt, N_trt, spp_count, group_count, experiment, monospecies, monogroup, sampling_num, water_trt, temp_trt, C.3, C.4, Forb, Legume, legume_spp, legume_num, trt, spp_trt, ACMI, ASTU, KOCR, LUPE, POPR, SONU, AGRE, ANGE, BRIN, PEVI, AMCA, BOGR, SCSC, SORI, ANCY, LECA))
+names(anppNoTrt)[names(anppNoTrt)=="ACMI1"] <- "Achillea.millefolium"
+names(anppNoTrt)[names(anppNoTrt)=="ASTU1"] <- "Asclepias.tuberosa"
+names(anppNoTrt)[names(anppNoTrt)=="KOCR1"] <- "Koeleria.cristata"
+names(anppNoTrt)[names(anppNoTrt)=="LUPE1"] <- "Lupinus.perennis"
+names(anppNoTrt)[names(anppNoTrt)=="POPR1"] <- "Poa.pratensis"
+names(anppNoTrt)[names(anppNoTrt)=="SONU1"] <- "Sorghastrum.nutans"
+names(anppNoTrt)[names(anppNoTrt)=="AGRE1"] <- "Agropyron.repens"
+names(anppNoTrt)[names(anppNoTrt)=="ANGE1"] <- "Andropogon.gerardi"
+names(anppNoTrt)[names(anppNoTrt)=="BRIN1"] <- "Bromus.inermis"
+names(anppNoTrt)[names(anppNoTrt)=="PEVI1"] <- "Petalostemum.villosum"
+names(anppNoTrt)[names(anppNoTrt)=="AMCA1"] <- "Amorpha.canescens"
+names(anppNoTrt)[names(anppNoTrt)=="BOGR1"] <- "Bouteloua.gracilis"
+names(anppNoTrt)[names(anppNoTrt)=="SCSC1"] <- "Schizachyrium.scoparium"
+names(anppNoTrt)[names(anppNoTrt)=="SORI1"] <- "Solidago.rigida"
+names(anppNoTrt)[names(anppNoTrt)=="ANCY1"] <- "Anemone.cylindrica"
+names(anppNoTrt)[names(anppNoTrt)=="LECA1"] <- "Lespedeza.capitata"
+names(anppNoTrt)[names(anppNoTrt)=="Unsort"] <- "Unsorted.Biomass"
+anppTrue16 <- melt(anppNoTrt, id.vars=c('plot', 'ring', 'year', 'month'), variable.name='species', value.name='anpp')
 
-
+#merge again with trt data
+anppTrue16Trt <- merge(anppTrue16, trt)
 
 
 
