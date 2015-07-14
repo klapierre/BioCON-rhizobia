6011265d15eddbe4592018ade72215aeecaf60a1
klapierre <lapierre.kimberly@gmail.com>
2015-07-14 16:42:43 -0700

    Calculate maximum cover between spring and fall dates for each year in each plot.

--- BioCON_cover data.R
+++ BioCON_cover data.R
@@ -2,3 +2,7 @@ setwd('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data')
 
 #cover data
 coverAll <- read.csv('e141_plant species percent cover.csv')
+
+#get max per year (across spring and fall)
+coverMax <- ddply(coverAll, c('year', 'plot', 'ring', 'CO2_trt', 'N_trt', 'spp_count', 'group_count', 'experiment', 'monospecies', 'monogroup', 'species'), summarise, cover=max(cover))
+
