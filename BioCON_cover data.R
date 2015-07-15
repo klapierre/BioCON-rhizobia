129963f884b32e26499aaac2cb377e1a6668a759
klapierre <lapierre.kimberly@gmail.com>
2015-07-14 17:36:02 -0700

    Removed water and temperature treatment plots.

--- BioCON_cover data.R
+++ BioCON_cover data.R
@@ -8,8 +8,11 @@ source('C:\\Users\\Kim\\Desktop\\BioCON rhizobia\\BioCON data\\BioCON-rhizobia\\
 #cover data
 coverAll <- read.csv('e141_plant species percent cover.csv')
 
+#remove the water and temperature treatment plots
+coverWater <- subset(coverAll, subset=(water_trt!='H2Oamb' & water_trt!='H2Oneg'))
+
 #remove all but the 16 species cover
-cover16 <- coverAll[(coverAll$species %in% c("Achillea millefolium","Asclepias tuberosa",
+cover16 <- coverWater[(coverWater$species %in% c("Achillea millefolium","Asclepias tuberosa",
                                              "Koeleria cristata","Lupinus perennis","Poa pratensis",
                                              "Sorghastrum nutans","Agropyron repens","Andropogon gerardi",
                                              "Bromus inermis","Petalostemum villosum","Amorpha canescens",
@@ -17,5 +20,5 @@ cover16 <- coverAll[(coverAll$species %in% c("Achillea millefolium","Asclepias t
                                              "Anemone cylindrica", "Lespedeza capitata")),]
 
 #get max per year (across spring and fall)
-coverMax <- ddply(coverAll, c('year', 'plot', 'ring', 'CO2_trt', 'N_trt', 'spp_count', 'group_count', 'experiment', 'monospecies', 'monogroup', 'species'), summarise, cover=max(cover))
+coverMax <- ddply(cover16, c('year', 'plot', 'ring', 'CO2_trt', 'N_trt', 'spp_count', 'group_count', 'experiment', 'monospecies', 'monogroup', 'species'), summarise, cover=max(cover))
 
