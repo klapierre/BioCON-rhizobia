b4489c37a1d9e4474e9ad3b2f689a9e6bfe2efd9
klapierre <lapierre.kimberly@gmail.com>
2015-11-25 18:49:46 -0800

    Calculating shoot and root biomass relative to the uninoculated controls within each CO2_trt and N_trt.

--- BioCON_whole soils benefit_harvest.R
+++ BioCON_whole soils benefit_harvest.R
@@ -11,7 +11,48 @@ plan <- read.csv('La Pierre_2015_CDR biocon_whole soil_plots.csv')%>%
   select(-replicate)
 names(plan)[names(plan)=="value"] <- "replicate"
 
+trt <- read.csv('e141_treatments.csv')
+
 #merge plan and harvest data to see what reps are missing (i.e., didn't grow in the field); also used for data check
-harvestAll <- merge(plan, harvest, by=c('plot', 'species', 'replicate'), all=T)%>%
-  group_by(plot, species, replicate)%>%
-  summarise(length=length(shoot_mass))
+harvestAll <- merge(plan, harvest, by=c('plot', 'species', 'replicate'), all=T)
+
+#merge harvest data with treatment information
+harvestTrt <- merge(harvestAll, trt, by=c('ring', 'plot'), all=T)%>%
+  filter(year!='NA')
+
+###calculate the relative biomass compared to uninoculated controls (percent difference)
+
+#subset out the soil inoculation controls and inoculated plants
+harvestCtl <- harvestTrt%>%filter(is.na(experiment))
+names(harvestCtl)[names(harvestCtl)=='shoot_mass'] <- 'shoot_mass_ctl'
+names(harvestCtl)[names(harvestCtl)=='root_mass'] <- 'root_mass_ctl'
+harvestCtlShoot <- harvestCtl%>%select(-root_mass_ctl)%>%
+  group_by(plot, species)%>%
+  summarise(shoot_mass_ctl=mean(shoot_mass_ctl))
+  harvestCtlShoot$CO2_trt <- with(harvestCtlShoot, ifelse(plot=='aCO2aN', 'Camb', ifelse(plot=='aCO2eN', 'Camb', 'Cenrich')))
+  harvestCtlShoot$N_trt <- with(harvestCtlShoot, ifelse(plot=='aCO2aN', 'Namb', ifelse(plot=='eCO2aN', 'Namb', 'Nenrich')))
+harvestCtlRoot <- harvestCtl%>%select(-shoot_mass_ctl)%>%
+  group_by(plot, species)%>%
+  summarise(root_mass_ctl=mean(root_mass_ctl))
+  harvestCtlRoot$CO2_trt <- with(harvestCtlRoot, ifelse(plot=='aCO2aN', 'Camb', ifelse(plot=='aCO2eN', 'Camb', 'Cenrich')))
+  harvestCtlRoot$N_trt <- with(harvestCtlRoot, ifelse(plot=='aCO2aN', 'Namb', ifelse(plot=='eCO2aN', 'Namb', 'Nenrich')))
+harvestCtlAll <- merge(harvestCtlShoot, harvestCtlRoot, by=c('plot', 'species', 'CO2_trt', 'N_trt'))%>%
+  select(-plot)
+
+#subset out inoculated plants
+harvestIno <- harvestTrt%>%filter(!is.na(experiment))
+
+#merge uninoculated controls back with inoculated plants, calculate relative cover, drop unneccesary columns
+harvestRel <- merge(harvestIno, harvestCtlAll, by=c('CO2_trt', 'N_trt', 'species'), all=T)%>%
+  mutate(shoot_mass_rel=((shoot_mass-shoot_mass_ctl)/shoot_mass_ctl), root_mass_rel=((root_mass-root_mass_ctl)/root_mass_ctl))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
