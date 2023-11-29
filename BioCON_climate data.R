6046d66fa275049b3937a7f1a3917cb6ec75be22
klapierre <kjkomatsu@uncg.edu>
2023-11-29 10:10:18 -0500

    Update climate data script to get newest data, convert to tidyverse, and calculate growing year climate summaries.

--- BioCON_climate data.R
+++ BioCON_climate data.R
@@ -1,56 +1,52 @@
 setwd('C:\\Users\\kjkomatsu\\OneDrive - UNCG\\NSF BioCON rhizobia\\data\\BioCON data')
 
-#climate data
-climate <- read.csv('e080_Daily climate summary.csv')
+##### Read in climate data ####
+climate <- read.csv('e080_Daily climate summary_2023.csv') %>% 
+  #create year, month, day columns
+  mutate(date=as.Date(as.character(Date), format='%m/%d/%Y'),
+         year=as.numeric(format(date, '%Y')),
+         month=as.factor(format(date, '%B')),
+         day=as.numeric(format(date, '%d'))) %>% 
+  #convert precip from in to cm
+  mutate(precip_cm=Precip.inches.*2.54) %>% 
+  #take average of min and max temps
+  mutate(avg_temp=(MinTemp.degF.+MaxTemp.degF.)/2)
 
-#get year,  month, day columns
-climate$date <- as.Date(as.character(climate$date), format='%m/%d/%Y') #make date column into a date
-climate$year <- as.numeric(format(climate$date, '%Y')) #year as numeric
-climate$month <- as.factor(format(climate$date, '%B')) #month in text format
-climate$day <- as.numeric(format(climate$date, '%d')) #day as numeric
 
-#convert precip from in to cm
-climate$precip_cm <- climate$precip_in*2.54
+#### Growing season climate summaries ####
+climateGrowingSeason <- climate %>% 
+  filter(month %in% c('May', 'June', 'July', 'August')) %>% 
+  group_by(year) %>% 
+  summarise(growing_precip_cm=sum(precip_cm),
+            growing_temp_min=mean(MinTemp.degF.),
+            growing_temp_max=mean(MaxTemp.degF.),
+            growing_temp=mean(avg_temp)) %>% 
+  ungroup()
 
-#take average of min and max temps
-climate$avg_temp <- (climate$temp_min+climate$temp_max)/2
 
-#get calendar year annual precip and mean max and min temp
-climateAnnual <- ddply(climate, c('year'), summarise,
-                       tot_precip_cm=sum(precip_cm),
-                       avg_temp_min=mean(temp_min),
-                       avg_temp_max=mean(temp_max),
-                       avg_temp=mean(avg_temp))
+#### Growing year climate summaries ####
+climateGrowingYear <- climate %>% 
+  mutate(growing_year=ifelse(month %in% c('September', 'October', 'November', 'December'), year+1,
+                             year)) %>% 
+  filter(growing_year!=2024) %>% 
+  group_by(growing_year) %>% 
+  summarise(annual_precip_cm=sum(precip_cm, na.rm=T),
+            avg_temp_min=mean(MinTemp.degF.),
+            avg_temp_max=mean(MaxTemp.degF.),
+            avg_temp=mean(avg_temp)) %>% 
+  ungroup() %>% 
+  rename(year=growing_year)
 
-#get growing season metrics
-climateDailyGrowingSeason <- subset(climate, subset=(gs=='growing'))
-climateGrowingSeason <- ddply(climateDailyGrowingSeason, c('year'), summarise,
-                              gs_precip_cm=sum(precip_cm),
-                              gs_temp_min=mean(temp_min),
-                              gs_temp_max=mean(temp_max),
-                              gs_temp=mean(avg_temp))
 
-#get growing year annual precip and mean max and min temp
-climateGrowingYear <- ddply(climate, c('gs_year'), summarise,
-                            gy_precip_cm=sum(precip_cm),
-                            gy_temp_min=mean(temp_min),
-                            gy_temp_max=mean(temp_max),
-                            gy_temp=mean(avg_temp))
-names(climateGrowingYear)[names(climateGrowingYear)=='gs_year'] <- 'year'
+#### Checking normality ####
+shapiro.test(climateGrowingSeason$growing_precip_cm)
+qqnorm(climateGrowingSeason$growing_precip_cm)
 
-#checking normality
-shapiro.test(climateAnnual$tot_precip_cm)
-qqnorm(climateAnnual$tot_precip_cm)
-
-shapiro.test(climateGrowingSeason$gs_precip_cm)
-qqnorm(climateGrowingSeason$gs_precip_cm)
-
-shapiro.test(climateGrowingYear$gy_precip_cm)
-qqnorm(climateGrowingYear$gy_precip_cm)
+shapiro.test(climateGrowingYear$annual_precip_cm)
+qqnorm(climateGrowingYear$annual_precip_cm)
 
 #merge all climate data together
-climateInitial <- merge(climateAnnual, climateGrowingSeason)
-climate <- merge(climateInitial, climateGrowingYear)
+climateSummary <- left_join(climateGrowingSeason, climateGrowingYear)
 
 #cleanup
-rm(list=c('climateAnnual', 'climateDailyGrowingSeason', 'climateGrowingSeason', 'climateGrowingYear', 'climateInitial'))
+rm(list=setdiff(ls(), "climateSummary"))
