25b8120b4316a5bea20864f85b62c88d67aa59bf
klapierre <kjkomatsu@uncg.edu>
2023-11-30 10:20:59 -0500

    Same analysis for cover data as biomass for curiosity sake. But cover is a poor proxy for biomass, so this isn't a good way to do it.

--- 4_BioCON_cover data.R
+++ 4_BioCON_cover data.R
@@ -5,6 +5,9 @@
 ################################################################################
 
 library(car)
+library(grid)
+library(nlme) 
+library(lsmeans)
 library(tidyverse)
 
 
@@ -164,3 +167,171 @@ coverANPPtotal <- coverANPP %>%
 coverANPPtotalModel <- lm(anpp_total~cover_total, data=coverANPPtotal)
 summary(coverANPPtotalModel)
 plot(coverANPPtotal$anpp_total~coverANPPtotal$cover_total)
+
+#####################################################################
+#### Overall, cover is a poor proxy for biomass for most species ####
+#####################################################################
+
+
+# #### Calculate Relative Yield and Relative Yield Total for each plot ####
+# 
+# #subset monocultures and drop non-focal species biomass
+# coverMono <- cover %>% 
+#   filter(spp_count==1, species2!='other') %>% 
+#   #calculate avearge species monoculture biomass for each year and CO2/N trt
+#   group_by(year, CO2_trt, N_trt, species2) %>% 
+#   summarise(mono_cover_mean=mean(cover)) %>% 
+#   ungroup() %>% 
+#   #remove species with 0 biomass in monoculture
+#   filter(mono_cover_mean>0)
+# 
+# #get list of species that should be present in each plot
+# sppList <- trt %>% 
+#   pivot_longer(Achillea.millefolium:Sorghastrum.nutans, names_to='species', values_to='species2') %>% 
+#   filter(species2!=0) %>% 
+#   select(plot, ring, species2, spp_count) %>% 
+#   unique()
+# 
+# #subset polycultures and calculate Relative Yield
+# coverExpected <- sppList %>% 
+#   left_join(trt) %>% 
+#   select(plot, ring, CO2_trt, N_trt, species2, spp_count) %>% 
+#   filter(spp_count>1) %>% 
+#   right_join(coverMono, relationship='many-to-many') %>% 
+#   #calculate relative yield for each species
+#   mutate(expected_spp_cover=ifelse(spp_count==4, mono_cover_mean/4,
+#                             ifelse(spp_count==9, mono_cover_mean/9,
+#                                    mono_cover_mean/16))) %>% 
+#   group_by(plot, ring, year) %>% 
+#   summarize(expected_cover=sum(expected_spp_cover)) %>% 
+#   ungroup()
+# 
+# coverPoly <- cover %>% 
+#   select(plot, ring, year, CO2_trt, N_trt, species2, spp_count, cover) %>% 
+#   filter(spp_count>1) %>% 
+#   right_join(coverMono) %>% 
+#   #calculate relative yield for each species
+#   mutate(expected_spp_cover=ifelse(spp_count==4, mono_cover_mean/4,
+#                             ifelse(spp_count==9, mono_cover_mean/9,
+#                                    mono_cover_mean/16))) %>% 
+#   mutate(RY=(cover/expected_spp_cover)) %>% 
+#   left_join(trt)
+# 
+# #calculate Relative Yield Total and total anpp per plot
+# RYT <- cover %>% 
+#   filter(spp_count>1) %>% 
+#   group_by(plot, ring, year) %>% 
+#   summarize(total_cover=sum(cover)) %>% 
+#   ungroup() %>% 
+#   left_join(coverExpected) %>% 
+#   mutate(RYT=total_cover/expected_cover) %>% 
+#   left_join(trt)
+# 
+# #check RYT and total anpp for normality within plots
+# # shapiro.test(anppPoly$RY)
+# qqnorm(coverPoly$RY)
+# 
+# shapiro.test(RYT$RYT)
+# qqnorm(RYT$RYT)
+# 
+# shapiro.test(RYT$total_cover)
+# qqnorm(RYT$total_cover)
+# 
+# #log10 transform for normality
+# coverPoly$logRY <- log10(coverPoly$RY)
+# # shapiro.test(anppPoly$logRY)
+# # qqnorm(anppPoly$logRY)
+# 
+# RYT$logRYT <- log10(RYT$RYT)
+# shapiro.test(RYT$logRYT)
+# qqnorm(RYT$logRYT)
+# 
+# RYT$log_total_cover <- log10(RYT$total_cover)
+# shapiro.test(RYT$log_total_cover)
+# qqnorm(RYT$log_total_cover)
+# 
+# 
+# #### Choose data subset ####
+# coverPolySubset <- subset(coverPoly, trt=='Camb_Namb')
+# RYTSubset <- subset(RYT, trt=='Camb_Namb')
+# 
+# 
+# #### Statistical Models - Relative Yield of legumes only ####
+# #note: subset only 2005 because that is the last year the plots were sorted to species in the dataset; drop Petalostemum because we don't have pot experiment data for that species (and it grows poorly in the plots as well)
+# summary(RYmodel <- lme(log(RY) ~ species2,
+#                        random=~1|plot,
+#                        data=subset(coverPolySubset, year==2005 &
+#                                    species2 %in% c('Amorpha canescens', 
+#                                                    'Lespedeza capitata', 
+#                                                    'Lupinus perennis'))))
+# anova(RYmodel)
+# lsmeans(RYmodel, ~species2)
+# 
+# #through time
+# summary(RYmodel <- lme(log(RY) ~ species2,
+#                        random=~1|year/plot,
+#                        data=subset(coverPolySubset, species2 %in% c('Amorpha canescens',
+#                                                                     'Lespedeza capitata',
+#                                                                     'Lupinus perennis'))))
+# anova(RYmodel)
+# lsmeans(RYmodel, ~species2)
+# 
+# #plot rhizobial specialization by year
+# ggplot(data=barGraphStats(data=subset(coverPolySubset, monogroup=='Legume' &
+#                                         species2 %in% c('Amorpha canescens','Lespedeza capitata','Lupinus perennis')),
+#                           variable="RY", byFactorNames=c("year", "species2")),
+#        aes(x=year, y=mean, color=species2)) +
+#   geom_point() +
+#   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2) +
+#   geom_hline(aes(yintercept=1), linetype="dashed") +
+#   xlab('Year') +
+#   ylab('Relative Yield in Field')
+#  
+# ggplot(data=barGraphStats(data=subset(coverPolySubset, monogroup=='Legume' & year==2005 &
+#                                         species2 %in% c('Amorpha canescens','Lespedeza capitata','Lupinus perennis')),
+#                           variable="RY", byFactorNames=c("species2")),
+#        aes(x=species2, y=mean, fill=species2)) +
+#   geom_bar(stat='identity') +
+#   geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=0.2) +
+#   geom_hline(aes(yintercept=1), linetype="dashed") +
+#   xlab('Legume Species') +
+#   ylab('Relative Yield in Field') +
+#   annotate('text', x=1, y=0.8, label='a', size=10) +
+#   annotate('text', x=2, y=0.78, label='a', size=10) +
+#   annotate('text', x=3, y=3.6, label='b', size=10) +
+#   scale_x_discrete(breaks=c('Amorpha canescens','Lespedeza capitata', 'Lupinus perennis'), labels=c('AMCA', 'LECA', 'LUPE')) +
+#   scale_fill_manual(values=c('#00760a', '#00760a', '#e46c0a'), breaks=c('Amorpha canescens','Lespedeza capitata', 'Lupinus perennis'), labels=c('specialist', 'specialist', 'generalist'))
+# #export at 700x500
+# 
+# 
+# #### Statistical Models - Relative Yield Total ####
+# #note: subset only 2005 because that is the last year the plots were sorted to species in the dataset
+# 
+# #4 spp polycultures only
+# summary(RYTmodel <- lme(log10(RYT) ~ as.factor(legume_num),
+#                         random=~1|year/plot,
+#                         data=subset(RYTSubset, legume_num %in% c(0,1,4) & spp_count==4)))
+# anova(RYTmodel)
+# lsmeans(RYTmodel, ~legume_num)
+# 
+# #plot rhizobial specialization by year
+# ggplot(data=barGraphStats(data=subset(RYTSubset, legume_num %in% c(0,1,4) & spp_count==4),
+#                           variable="RYT", byFactorNames=c("year", "legume_num")),
+#        aes(x=year, y=mean, color=as.factor(legume_num))) +
+#   geom_point(position=position_dodge(0.9)) +
+#   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(0.9)) +
+#   geom_hline(aes(yintercept=1), linetype="dashed") +
+#   xlab('Year') +
+#   ylab('Relative Yield in Field')
+# 
+# ggplot(data=barGraphStats(data=subset(RYTSubset, legume_num %in% c(0,1,4) & spp_count==4), variable="RYT", byFactorNames=c("legume_num")),
+#        aes(x=as.factor(legume_num), y=mean)) +
+#   geom_bar(stat='identity') +
+#   geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=0.2) +
+#   geom_hline(aes(yintercept=1), linetype="dashed") +
+#   xlab('Legume Number') +
+#   ylab('Relative Yield Total') +
+#   annotate('text', x=1, y=2.0, label='a', size=10) +
+#   annotate('text', x=2, y=2.0, label='a', size=10) +
+#   annotate('text', x=3, y=1.75, label='b', size=10)
+# #export at 700x500
